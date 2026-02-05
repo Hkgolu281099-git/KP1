54b50bc55b9ecf5e60aa19a2858bb7dc34a413fa
Harendra Kmar Golu Raj <rajhk281099@gmail.com>
2026-02-05 10:15:01 +0530

    both file saved

---
+++ particula set.py
@@ -0,0 +1,13 @@
+# #particular
+
+# a={"Shubh","Priyanshu","Abhi","Sam"}
+# b=0
+# # for item in  a:
+# #  if item == "Shubh":
+# #    print("Shubh")
+# b=list(a)
+# print(list(b))
+# print(b[0])
+# print(b[1])
+# print(b[2])
+# print(b[3])
